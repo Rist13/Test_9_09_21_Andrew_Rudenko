bf59cc5e20221f1ba1d7a2bca1cf4c892c2aa9a0
Rist88 <holistic.rist@gmail.com>
2021-09-09 21:06:35 +0300

    fix exercise 5

--- 05-largestPairSum/index.js
+++ 05-largestPairSum/index.js
@@ -4,14 +4,23 @@
  * @returns {number} - returns the new number
  */
 export const largestPairSum = (numbers) => {
-    let high1 = numbers[0], high2 = numbers[0];
-    for (let a of numbers) {
-        if (a>high1) {
-            high1 = a;
-        }
-        if (a>high1 && a>high2) {
-            high2 = a;
-        }
+    let high1 = 0,
+    high2 = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > numbers[high1]) {
+      high1 = i;
     }
-    return high1 + high2; 
+  }
+  if (high1 === 0) {
+    high2 = 1;
+  }
+  for (let i = high2; i < numbers.length; i++) {
+    if (i === high1) {
+      continue;
+    }
+    if (numbers[i] > numbers[high2]) {
+      high2 = i;
+    }
+  }
+  return numbers[high1] + numbers[high2];
 }
