91e7f222d2692a891707615c0bb1495e8e1498be
Rist88 <holistic.rist@gmail.com>
2021-09-10 02:42:19 +0300

    Do exercise 8 (finally going to sleep :)

--- 08-chunk/index.js
+++ 08-chunk/index.js
@@ -1,5 +1,17 @@
 // TODO: rename function, maybe array-of-arrays
 
 export const chunk = (arr = [], size = 0) => {
-
+    let out = [];
+    if (size === 0) {
+        return out;
+    }
+    let chunkNum = Math.floor(arr.length / size),
+        leTrunc = arr.length - chunkNum * size;
+    for (let i = 0, j = 0; i < arr.length && j < chunkNum; i += size, j++) {
+        out = [...out, arr.slice(i, i + size)];
+    }
+    if (leTrunc !== 0) {
+        out = [...out, arr.slice(-leTrunc)];
+    }
+    return out;
 };
